9bca227a9b9a34282a95ec5356fc4087aacb6538
VladRafli <rafli.jaskandi@gmail.com>
2020-05-25 19:19:38 +0700

    Source code created...

---
+++ Wicked.c
@@ -0,0 +1,54 @@
+/*--------------------------------------------------
+ * Wicked App
+ * 
+ * App that used to secure your account information
+ * by hashing, bcrypt, then encryption.
+ * This is an example of Dropbox database
+ * protection aginst hackers
+ * 
+ * Version of this app: v.1.0.0 (Alpha Test)
+ ---------------------------------------------------*/
+
+/*--------------------
+ * Includes
+ ---------------------*/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <conio.h>
+
+/* --- If C++ --- */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*--------------------
+ * Defines
+ ---------------------*/
+
+//Manual Debuging
+// #define DEBUG
+
+/*---------------------
+ * Function Prototypes
+ ----------------------*/
+
+
+
+/*---------------------
+ * Functions
+ ----------------------*/
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World\n");
+    getch();
+    return 0;
+}
+
+/* --- If C++ --- */
+#ifdef __cplusplus
+}
+#endif
